95eb517a6222386b674cb5c8c7733e95ffbe9622
SandunUCSC <sandunchathuranga202@gmail.com>
2024-07-14 13:26:27 +0530

    albumitem and songitem components created

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -1,12 +1,48 @@
-import React from 'react'
-import Navbar from './Navbar'
+import React from "react";
+import Navbar from "./Navbar";
+import { albumsData } from "../assets/assets";
+import AlbumItem from "./AlbumItem";
+import { songsData } from "../assets/assets";
 
 function DisplayHome() {
   return (
-   <>
-<Navbar />
-   </>
-  )
+    <>
+      <Navbar />
+      <div className="mb-4">
+        <h1 className="my-5 text-2xl font-bold">Featured Charts</h1>
+        <div className="flex overflow-auto">
+          {albumsData.map((item, index) => (
+            <AlbumItem
+              key={index}
+              name={item.name}
+              desc={item.desc}
+              id={item.id}
+              image={item.image}
+            />
+          ))}
+        </div>
+      </div>
+
+
+      <div className="mb-4">
+        <h1 className="my-5 text-2xl font-bold">Today's biggest hits</h1>
+        <div className="flex overflow-auto">
+        {songsData.map((item, index) => (
+            <AlbumItem
+              key={index}
+              name={item.name}
+              desc={item.desc}
+              id={item.id}
+              image={item.image}
+            />
+          ))}
+        </div>
+      </div>
+
+
+
+    </>
+  );
 }
 
-export default DisplayHome
+export default DisplayHome;
---
+++ src/components/SongItem.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SongItem = ({name,image,desc,id}) => {
+  return (
+    <div className='min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]'>
+       <p className='mt-2 mb-1 font-bold'>{name}</p>
+       <p className='text-sm text-slate-200'>{desc}</p>
+    </div>
+  )
+}
+
+export default SongItem
