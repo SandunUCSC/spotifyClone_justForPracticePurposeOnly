eff5a3ab722c29394100cbbbfcaeaec586af7367
SandunUCSC <sandunchathuranga202@gmail.com>
2024-07-14 21:17:59 +0530

    display individual album data

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import DisplayHome from './DisplayHome'
+import DisplayAlbum from './DisplayAlbum'
 
 function Display() {
   return (
     <div className='w-[100%] overflow-auto m-2 pt-4 px-6 rounded bg-[#121212] text-white lg:w-[75%] lg:ml-0'>
       <Routes>
         <Route path='/' element={<DisplayHome/>} />
+        <Route path='/album/:id' element={<DisplayAlbum/>} />
       </Routes>
     </div>
   )
